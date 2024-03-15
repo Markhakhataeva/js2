ef2c955226af7416234cb64e953b86e43412d622
mike <mike@hk>
2024-03-15 22:16:42 +0300

    radg

---
+++ 2.js
@@ -0,0 +1,146 @@
+//1 
+
+let a =5;
+let b = 3;
+let c=a+b;
+console.log(c)
+
+//2
+
+let d=a*b;
+console.log(d)
+
+
+//3
+
+
+let f=3;
+
+let g=(f<0)
+let r=(f>0)
+console.log(r)
+
+
+//4
+
+
+let h=a-b;
+let j=a%b;
+b--
+a++;
+
+console.log(b)
+
+//5
+
+
+
+let x=true;
+let y=false;
+let n=x<y;
+let nn=x>y;
+let nnn=x==y;
+let nnnn=x!=y;
+console.log(n)
+
+//6
+
+
+ let age=16;
+
+ if(age<18){
+    console.log("Вы несовершеннолетний")
+ }else{
+    console.log("Вы совершеннолетний")
+ }
+
+
+
+ //7
+
+ function squareArray (numbers){
+    const result =[];
+
+    for(let i = 0; i < numbers.length; i++){
+        result.push(numbers[i]**2);
+
+    }
+    return result;
+ }
+ const squares=squareArray([3,5,6])
+
+
+ console.log(squares)
+
+
+ //8
+
+
+let counter=0;
+counter+=5
+
+console.log(counter)
+
+
+//9
+
+
+let text="hi hi";
+let text2="hello";
+
+let mm=text==text2;
+let mb=text!=text2;
+console.log(mb)
+
+
+
+//10
+
+const arr = [2,3,4,5];
+
+for( let i=0;i< arr.length;i++)[
+    console.log(arr[i])
+]
+
+
+
+//11
+
+
+let agee=18;
+let isAdult=(age<=18)
+console.log(isAdult)
+
+
+//1js
+
+let Age=prompt("Ваш возраст?")
+
+if(Age<18){
+    console.log("ВЫ не можете пройти тестирование"
+    )
+}else{
+    console.log("ВЫ можете пройти тестирование")
+}
+
+
+
+
+//2js
+
+
+let= ["я не поняла это задание"]
+
+
+//функция
+
+
+
+function sum (a,b){
+    return a + b
+}
+
+const result = sum( 10,10)
+console.log(result)
+
+//////я дальше не поняла /'_'///
